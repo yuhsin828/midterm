a98d5144a8cb2b0e7659876d9cd6e5d42a4cb6a7
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2022-04-21 00:38:35 +0800

    t02 layout ok

--- js/album.js
+++ js/album.js
@@ -1,197 +1,28 @@
-let album = {};
-album.ID = '110819018'
-album.imgurl = '5727775';
-album.name = 'album';
-album.info = 'tsai album';
-album.photoNum = '20';
-album.videoNum = '0';
-album.viewNum = '888';
-album.author = '蔡';
-album.authorImg = 'https://randomuser.me/api/portraits/women/8.jpg';
-album.images = [];
+var url = 'https://script.google.com/macros/s/AKfycbyAdQ9c8vbq4W2amts6CfrIc_bz49QOULzWICPUibuRlOfSMvxG67uYE5ZZQOY8AjmJ/exec';
 
-let ary = [];
-let obj = {};
-obj.ID = "5693382"
-obj.imgurl = "5693382";
-obj.title = "孩子們躺在白色的床上";
-obj.author = "Alex Green";
-obj.likeNum = "154";
-obj.commentNum = "355";
-ary.push(obj);
+// let url = "https://www.google.com/?name=Ben&from=taipei&job=teacher";
+// let argumentAry = url.split('?');
+// console.log(argumentAry);
+// let argumentAry2 = argumentAry[1].split('&');
+// // console.log(argumentAry2);
+// for (let i = 0; i < argumentAry2.length; i++) {
+//     argumentAry2[i] = argumentAry2[i].split('=');
+// }
+// console.log(argumentAry2);
 
-obj = {};
-obj.ID = "3763578";
-obj.imgurl = "3763578";
-obj.title = "一個幸福的家庭的照片";
-obj.author = "Jonathan Borba";
-obj.likeNum = "96";
-obj.commentNum = "563";
-ary.push(obj);
+let pageID = document.location.toString().split('?')[1];
 
-obj = {};
-obj.ID = "4545207";
-obj.imgurl = "4545207";
-obj.title = "父母與兩個小孩與狗";
-obj.author = "Ketut Subiyanto";
-obj.likeNum = "162";
-obj.commentNum = "507";
-ary.push(obj);
+let param = {};
+param.method = 'getFakeAPI';
+param.uid = '110819018';
+param.token = 'smXciVKZ1fMyUb';
+param.id = "pageID";
 
-obj = {};
-obj.ID = "3768164";
-obj.imgurl = "3768164";
-obj.title = "給曲奇餅和杯熱的飲料的愉快的年邁的婦女在家做家庭作業的喜悅的孫女";
-obj.author = "Andrea Piacquadio";
-obj.likeNum = "126";
-obj.commentNum = "791";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4609049";
-obj.imgurl = "4609049";
-obj.title = "爸爸抱起媽媽";
-obj.author = "Vlada Karpovich";
-obj.likeNum = "189";
-obj.commentNum = "816";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4259707";
-obj.imgurl = "4259707";
-obj.title = "母親和女兒準備鱷梨吐司";
-obj.author = "August de Richelieu";
-obj.likeNum = "424";
-obj.commentNum = "291";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4473782";
-obj.imgurl = "4473782";
-obj.title = "孩子們親吻他們的母親的照片";
-obj.author = "Ketut Subiyanto";
-obj.likeNum = "87";
-obj.commentNum = "431";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4259138";
-obj.imgurl = "4259138";
-obj.title = "家庭在廚房裡做早餐";
-obj.author = "August de Richelieu";
-obj.likeNum = "167";
-obj.commentNum = "685";
-ary.push(obj);
-
-obj = {};
-obj.ID = "3807337";
-obj.imgurl = "3807337";
-obj.title = "家人在一起準備薄餅";
-obj.author = "Elina Fairytale";
-obj.likeNum = "129";
-obj.commentNum = "608";
-ary.push(obj);
-
-obj = {};
-obj.ID = "7983157";
-obj.imgurl = "7983157";
-obj.title = "兩個爸爸";
-obj.author = "Kampus Production";
-obj.likeNum = "3";
-obj.commentNum = "168";
-ary.push(obj);
-
-obj = {};
-obj.ID = "3933091";
-obj.imgurl = "3933091";
-obj.title = "和孩子一起玩的人的照片";
-obj.author = "Tatiana Syrikova";
-obj.likeNum = "106";
-obj.commentNum = "637";
-ary.push(obj);
-
-obj = {};
-obj.ID = "7114420";
-obj.imgurl = "7114420";
-obj.title = "一家五口坐在沙發上";
-obj.author = "Monstera";
-obj.likeNum = "74";
-obj.commentNum = "247";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4260096";
-obj.imgurl = "4260096";
-obj.title = "幸福的家庭，在廚房裡";
-obj.author = "August de Richelieu";
-obj.likeNum = "103";
-obj.commentNum = "39";
-ary.push(obj);
-
-obj = {};
-obj.ID = "7946449";
-obj.imgurl = "7946449";
-obj.title = "爸爸與說故事爸爸";
-obj.author = "Karolina Grabowska";
-obj.likeNum = "7";
-obj.commentNum = "243";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4624879";
-obj.imgurl = "4624879";
-obj.title = "小孩看著媽媽辦公";
-obj.author = "Ivan Samkov";
-obj.likeNum = "72";
-obj.commentNum = "212";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4544901";
-obj.imgurl = "4544901";
-obj.title = "爸爸與兩個兒子";
-obj.author = "Ketut Subiyanto";
-obj.likeNum = "22";
-obj.commentNum = "834";
-ary.push(obj);
-
-obj = {};
-obj.ID = "3806953";
-obj.imgurl = "3806953";
-obj.title = "女人和她的孩子一起玩的照片";
-obj.author = "yahoo.com";
-obj.likeNum = "311";
-obj.commentNum = "178";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4473871";
-obj.imgurl = "4473871";
-obj.title = "女人和她的孩子躺在床上的照片";
-obj.author = "Ketut Subiyanto";
-obj.likeNum = "109";
-obj.commentNum = "502";
-ary.push(obj);
-
-obj = {};
-obj.ID = "4545197";
-obj.imgurl = "4545197";
-obj.title = "父母與小孩躺在床上";
-obj.author = "Ketut Subiyanto";
-obj.likeNum = "55";
-obj.commentNum = "117";
-ary.push(obj);
-
-obj = {};
-obj.ID = "5793919";
-obj.imgurl = "5793919";
-obj.title = "三個女人相擁";
-obj.author = "Yan Krukov";
-obj.likeNum = "77";
-obj.commentNum = "311";
-ary.push(obj);
-
-album.images = ary;
-
-let albumStr = JSON.stringify(album);
-console.log(albumStr);
+$.post(url, param, function (data) {
+    console.log("success");
+    console.log(data);
+    //   initPage(data);
+}).fail(function () {
+    console.log("fail");
+    console.log(data);
+});
--- js/script.js
+++ js/script.js
@@ -1,64 +1,48 @@
-const imgStr = 'https://images.pexels.com/photos/ID_HERE/pexels-photo-ID_HERE.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
-let imgAry = [5693382, 3763578, 4545207, 3768164, 4609049, 4259707, 4473782, 4259138, 3807337, 7983157, 3933091, 7114420, 4260096, 7946449, 4624879, 4544901, 3806953, 4473871, 4545197, 5793919];
-let textAry = ['album1', 'album2', 'album3', 'album4', 'album5', 'album6', 'album7', 'album8', 'album9', 'album10', 'album11', 'album12', 'album13', 'album14', 'album15', 'album16', 'album17', 'album18', 'album19', 'album20'];
+// const imgStr = 'https://images.pexels.com/photos/ID_HERE/pexels-photo-ID_HERE.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+// let imgAry = [5693382, 3763578, 4545207, 3768164, 4609049, 4259707, 4473782, 4259138, 3807337, 7983157, 3933091, 7114420, 4260096, 7946449, 4624879, 4544901, 3806953, 4473871, 4545197, 5793919];
+// let textAry = ['album1', 'album2', 'album3', 'album4', 'album5', 'album6', 'album7', 'album8', 'album9', 'album10', 'album11', 'album12', 'album13', 'album14', 'album15', 'album16', 'album17', 'album18', 'album19', 'album20'];
+var url = 'https://script.google.com/macros/s/AKfycbyAdQ9c8vbq4W2amts6CfrIc_bz49QOULzWICPUibuRlOfSMvxG67uYE5ZZQOY8AjmJ/exec';
+
+let pageID = document.location.toString().split('?')[1];
 
 $(document).ready(function () {
-    imgAry.map(function (value, index) {
-        $('section .row').append(t01(value, textAry[index]));
-    });
+    // imgAry.map(function (value, index) {
+    //     $('section .row').append(t01(value, textAry[index]));
+    // });
+    getData();
 });
 
+function getData() {
+    let param = {};
+    param.method = 'getFakeAPI';
+    param.uid = '110819018';
+    param.token = 'smXciVKZ1fMyUb';
+    param.id = pageID;
+    $.post(url, param, function (data, textStatus, xhr) {
+        console.log("success");
+        console.log(data);
+        // initPage(data);
+    }).fail(function () {
+        console.log("fail");
+        console.log(data);
+    });
+}
 
-
-// var url = 'https://script.google.com/macros/s/AKfycbyAdQ9c8vbq4W2amts6CfrIc_bz49QOULzWICPUibuRlOfSMvxG67uYE5ZZQOY8AjmJ/exec';
-// let param = {};
-// param.method = 'getFakeAPI';
-// param.uid = '110819018';
-// param.token = 'sCNmJc58HxJw1p';
-// param.id = "110819018";
-// $.post(url, parameter, function(data) {
-//   console.log("success");
-//   console.log(data);
-//   initPage(data);
-// }).fail(function() {
-//   console.log("fail");
-// });
-
-
-// var url = "";
-// var parameter = {};
-// var jqxhr = $.post(url, parameter, function(data) {
-//   console.log("success");
-//   console.log(data);
-//   initPage(data);
-// })
-// .done(function(data) {
-//   console.log( "second success");
-//   console.log(data)
-// })
-// .fail(function() {
-//   console.log( "error" );
-// })
-// .always(function() {
-//   console.log( "finished" );
-// });
-
-
-function t01(ID_HERE, TEXT_HERE) {
+function t01(ALBUM_ID, IMG_ID, ALBUM_NAME, PHOTO_NUM, VIEW_NUM) {
     let html =
-    `
+        `
         <div class="col-xl-3 col-md-4 col-sm-6 pt-3">
             <div class="imgbox position-relative hz-280">
-                <a class="img position-absolute" href="./album.html">
-                    <img src="https://images.pexels.com/photos/${ID_HERE}/pexels-photo-${ID_HERE}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                <a class="img position-absolute" href="./album.html?${ALBUM_ID}">
+                    <img src="https://images.pexels.com/photos/${IMG_ID}/pexels-photo-${IMG_ID}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                         class="w-100 h-100 object-fit-cover position-absolute img-fluid" alt="">
                 </a>
-                <div class="info position-absolute fz-16 py-0">
-                    <div class="line line1">${TEXT_HERE}</div>
-                    <div class="line line2">N張相片，NNN次檢視</div>
-                    <div class="line line3">
-                        <i class="fa fa-share"></i>
-                        <i class="fa fa-download"></i>
+                <div class="info position-absolute fz-16 p-2 text-light">
+                    <div class="line line1">${ALBUM_NAME}</div>
+                    <div class="line line2">${PHOTO_NUM}張相片，${VIEW_NUM}次檢視</div>
+                    <div class="line line3 text-end">
+                        <a href="#"><i class="fa fa-share mx-1 text-light"></i></a>
+                        <a href="#"><i class="fa fa-download mx-1 text-light"></i></a>
                     </div>
                 </div>
             </div>
